d285096bab9641df45b45af2c21603986d2f26d4
Uskov Valerii <uskovvospb@gmail.com>
2021-02-13 23:50:56 +0300

    deleted Test file

--- src/JavaRush/JavaCore/Level14/Lesson8/Test.java
+++
@@ -1,9 +0,0 @@
-package JavaRush.JavaCore.Level14.Lesson8;
-
-public class Test {
-    public static void main(String[] args) {
-        System.out.println("Hello World");
-
-        System.out.println("Say Hello");
-    }
-}
