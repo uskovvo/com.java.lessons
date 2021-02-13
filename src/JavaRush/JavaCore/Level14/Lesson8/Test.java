ee47774c9d5b44761dc7bb2393cf46cf6ee1e145
Uskov Valerii <uskovvospb@gmail.com>
2021-02-13 23:29:59 +0300

    change Test file. Addede "Say Hello"

--- src/JavaRush/JavaCore/Level14/Lesson8/Test.java
+++ src/JavaRush/JavaCore/Level14/Lesson8/Test.java
@@ -3,5 +3,7 @@ package JavaRush.JavaCore.Level14.Lesson8;
 public class Test {
     public static void main(String[] args) {
         System.out.println("Hello World");
+
+        System.out.println("Say Hello");
     }
 }
